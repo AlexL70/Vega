ab88b92f1984223fa1c5ef6cb801a1389424b495
AlexL70 <alexander.levinson.70@gmail.com>
2019-05-01 15:38:07 +0300

    Add Make, Model and Feature models; create first migration

---
+++ Models/Make.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Vega.Models
+{
+    public class Make
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public DbSet<Model> Models { get; set; }
+    }
+}
--- Models/VegaDbContext.cs
+++ Models/VegaDbContext.cs
@@ -4,9 +4,12 @@ namespace Vega.Models
 {
     public class VegaDbContext : DbContext
     {
+        public DbSet<Make> Makes { get; set; }
+        public DbSet<Model> Models { get; set; }
+        public DbSet<Feature> Features { get; set; }
+
         public VegaDbContext(DbContextOptions<VegaDbContext> options) : base(options)
         {
-            
         }
     }
 }
