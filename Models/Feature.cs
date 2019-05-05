b6cfb4ff49c4a0e2840d9adac856e0896aa06d15
AlexL70 <alexander.levinson.70@gmail.com>
2019-05-05 13:25:06 +0300

    46-49. Update domain model: add Vehicle entity and Vehicle-Feature many-to-many relation

--- Models/Feature.cs
+++ Models/Feature.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace Vega.Models
@@ -7,5 +9,11 @@ namespace Vega.Models
         public int Id { get; set; }
         [Required] [StringLength(255)]
         public string Name { get; set; }
+        public ICollection<VehicleFeature> Vehicles { get; set; }
+
+        public Feature()
+        {
+            Vehicles = new Collection<VehicleFeature>();
+        }
     }
 }
--- Models/VegaDbContext.cs
+++ Models/VegaDbContext.cs
@@ -7,9 +7,13 @@ namespace Vega.Models
         public DbSet<Make> Makes { get; set; }
         public DbSet<Model> Models { get; set; }
         public DbSet<Feature> Features { get; set; }
+        public DbSet<Vehicle> Vehicles { get; set; }
 
-        public VegaDbContext(DbContextOptions<VegaDbContext> options) : base(options)
-        {
+        public VegaDbContext(DbContextOptions<VegaDbContext> options) : base(options) {}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            modelBuilder.Entity<VehicleFeature>()
+                .HasKey(vf => new { vf.VehicleId, vf.FeatureId});
         }
     }
 }
