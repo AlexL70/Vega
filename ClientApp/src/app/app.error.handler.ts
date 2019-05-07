3956e6394e258d4de9a62ffc48fc779d4dc4f217
AlexL70 <alexander.levinson.70@gmail.com>
2019-05-07 21:23:58 +0300

    88. Zones in Angular

--- ClientApp/src/app/app.error.handler.ts
+++ ClientApp/src/app/app.error.handler.ts
@@ -1,17 +1,20 @@
 import { ToastyService } from 'ng2-toasty';
-import { ErrorHandler, Inject } from "@angular/core";
+import { ErrorHandler, Inject, NgZone } from "@angular/core";
 
 export class AppErrorHandler implements ErrorHandler {
-  constructor(@Inject(ToastyService) private toastyService: ToastyService) {}
+  constructor(@Inject(ToastyService) private toastyService: ToastyService,
+    private ngZone: NgZone) {}
 
   handleError(error: any): void {
-    console.log("Error from central error handler:", error);
-    this.toastyService.error({
-      title: 'Error',
-      msg: 'Unexpected server error occured while saving vehicle',
-      theme: 'bootstrap',
-      showClose: true,
-      timeout: 5000
+    //console.log("Error from central error handler:", error);
+    this.ngZone.run(() => {
+      this.toastyService.error({
+        title: 'Error',
+        msg: 'Unexpected server error occured',
+        theme: 'bootstrap',
+        showClose: true,
+        timeout: 5000
+      });
     });
   }
 }
