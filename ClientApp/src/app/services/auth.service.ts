6ff6947958140b7c2f2a142c7e12d77fe2faf03a
AlexL70 <alexander.levinson.70@gmail.com>
2019-06-02 17:42:57 +0300

    150. Refactoring

--- ClientApp/src/app/services/auth.service.ts
+++ ClientApp/src/app/services/auth.service.ts
@@ -4,61 +4,55 @@ import Auth0Lock from 'auth0-lock';
 
 Injectable()
 export class Auth {
-  private readonly token = 'token';
+  private readonly tokenKey = 'token';
+  private readonly profileKey = 'profile';
   lock: Auth0LockStatic;
   profile: any;
   private roles: string[] = [];
+  //  lock configuration options
+  private readonly lockOptions: Auth0LockConstructorOptions = {
+    auth: {
+      responseType: 'token id_token',
+      audience: 'https://api.vega.com',
+      redirectUrl: 'https://localhost:5001',
+      params: {
+        scope: 'openid email profile'
+      }
+    },
+    additionalSignUpFields: [
+      {
+        name: "name",
+        placeholder: "Name"
+      }
+    ],
+    autoclose: true,
+    oidcConformant: true
+  };
 
   constructor() {
-    //  lock configuration options
-    var options = {
-      auth: {
-        responseType: 'token id_token',
-        audience: 'https://api.vega.com',
-        redirectUrl: 'https://localhost:5001',
-        params: {
-          scope: 'openid email profile'
-        }
-      },
-      additionalSignUpFields: [
-        {
-          name: "name",
-          placeholder: "Name"
-        }
-      ],
-      autoclose: true,
-      oidcConformant: true
-    };
     //  Configure Auth0
-    this.lock = new Auth0Lock('dfyTdzt05ACS3z82F65bLcjrg84FYNbO', 'dev-zlon.eu.auth0.com', options);
-    //  Get user roles from the token
-    this.extractRoles();
-    //  Get user profile from the local storage if any
-    this.profile = JSON.parse(localStorage.getItem('profile'));
-    // if(this.profile)
-    //   console.log("User profile got from the local storage: ", this.profile);
+    this.lock = new Auth0Lock('dfyTdzt05ACS3z82F65bLcjrg84FYNbO', 'dev-zlon.eu.auth0.com', this.lockOptions);
+    //  Read auth data from local storage if any
+    this.readAuthDataFromLocalStorage();
     //  Add callback for lock `authenticated` event
-    this.lock.on('authenticated', (authResult) => {
-      // console.log('AuthResult', authResult);
-      localStorage.setItem(this.token, authResult.accessToken);
+    this.lock.on('authenticated', (authResult) => this.onUserAuthenticated(authResult));
+  }
 
-      this.extractRoles();
-
-      this.lock.getUserInfo(authResult.accessToken, (error, profile) => {
-        if (error) {
-          throw error;
-        }
-        if (profile) {
-          // console.log("User profile: ", profile);
-          localStorage.setItem('profile', JSON.stringify(profile));
-          this.profile = profile;
-        }
-      });
+  private onUserAuthenticated(authResult: AuthResult) {
+    localStorage.setItem(this.tokenKey, authResult.accessToken);
+    this.lock.getUserInfo(authResult.accessToken, (error, profile) => {
+      if (error) {
+        throw error;
+      }
+      if (profile) {
+        localStorage.setItem(this.profileKey, JSON.stringify(profile));
+      }
+      this.readAuthDataFromLocalStorage();
     });
   }
 
-  private extractRoles() {
-    let tkn = localStorage.getItem(this.token);
+  private readAuthDataFromLocalStorage() {
+    let tkn = localStorage.getItem(this.tokenKey);
     if(tkn) {
       let jwtHelper = new JwtHelper();
       let tknDecoded = jwtHelper.decodeToken(tkn);
@@ -67,6 +61,9 @@ export class Auth {
       if (isDevMode)
         console.log("User roles: ", this.roles);
     }
+    this.profile = JSON.parse(localStorage.getItem(this.profileKey));
+    if (isDevMode)
+      console.log("User profile:", this.profile);
   }
 
   public hasRole(role: string) {
@@ -81,13 +78,13 @@ export class Auth {
   public authenticated(): boolean {
     //  Check if there is unexpired JWT
     //  This searches for an item in localStorage with key == this.token
-    return tokenNotExpired(this.token);
+    return tokenNotExpired(this.tokenKey);
   }
 
   public logout(): void {
     //  Remove token from localStorage
-    localStorage.removeItem(this.token);
-    localStorage.removeItem('profile');
+    localStorage.removeItem(this.tokenKey);
+    localStorage.removeItem(this.profileKey);
     this.profile = null;
     this.roles = [];
   }
